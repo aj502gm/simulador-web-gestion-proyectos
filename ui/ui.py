bbb4f6fd047b41245d3c63578e4fcbed6a3720b2
Rene-Tarot <rene.tarot@galileo.edu>
2025-08-11 22:21:10 -0600

    template del formulario

--- ui/ui.py
+++ ui/ui.py
@@ -6,9 +6,23 @@ import streamlit as st
 
 def project_input_form():
     st.subheader("Definición del Proyecto")
-    name = st.text_input("Nombre del proyecto")
-    activities = st.text_area("Actividades (JSON)")
-    return name, activities
+    
+    with st.form("input_form"):
+        name = st.text_input("Nombre del proyecto")
+        file = st.file_uploader("Subir archivo .csv", type=["csv"])
+        st.markdown("""
+                    **Formato esperado del archivo CSV:**
+
+                    ```csv
+                    id,name,optimistic_duration,most_likely_duration,pessimistic_duration,optimistic_cost,most_likely_cost,pessimistic_cost,dependencies
+                    A1,Diseño,2,4,6,500,800,1000,
+                    A2,Construcción,10,14,20,2000,2500,3000,A1
+                    ```
+                    """)
+
+        send = st.form_submit_button("Subir")
+
+    return name, file
 
 def montecarlo_results_chart(df):
     st.subheader("Resultados de Simulación Monte Carlo")
